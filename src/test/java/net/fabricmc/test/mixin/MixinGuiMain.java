6de8011f0e5301d6d3b42efde3f90e2d697be2ed
Shadowfacts <me@shadowfacts.net>
2016-08-23 15:09:58 -0400

    Fix test mod for latest mappings

--- src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
+++ src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
@@ -17,7 +17,7 @@
 package net.fabricmc.test.mixin;
 
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.impl.GuiMainMenu;
+import net.minecraft.client.gui.screen.menu.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
