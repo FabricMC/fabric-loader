0ea32ba1e292f76c3f8063c4152fedfe8960c217
Aesen Vismea <aesen@unascribed.com>
2016-08-25 16:21:38 -0400

    Create the Display extremely early in the tweaker (#17)
    * Create the Display extremely early in the tweaker
    
    This builds a foundation for the loading screen later, and improves perceived responsiveness.
    
    * Use --version for the window title

--- src/main/java/net/fabricmc/base/launch/FabricClientTweaker.java
+++ src/main/java/net/fabricmc/base/launch/FabricClientTweaker.java
@@ -21,15 +21,28 @@ import net.fabricmc.base.loader.MixinLoader;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 
+import com.google.common.base.Throwables;
+import com.google.common.io.Files;
+
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 public class FabricClientTweaker implements ITweaker {
 
@@ -64,6 +77,65 @@ public class FabricClientTweaker implements ITweaker {
             if (arg.startsWith("--")) {
                 this.args.put(arg, args.get(i + 1));
             }
+        }
+        
+        createEarlyDisplay();
+    }
+
+    private void createEarlyDisplay() {
+        File gameDir = new File(args.get("--gameDir"));
+        Properties def = new Properties();
+        def.setProperty("fullscreen", "false");
+        def.setProperty("overrideWidth", "0");
+        def.setProperty("overrideHeight", "0");
+        Properties props = new Properties(def);
+        try {
+            props.load(Files.newReader(new File(gameDir, "options.txt"), StandardCharsets.UTF_8));
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        boolean fullscreen = args.containsKey("--fullscreen") || Boolean.parseBoolean(props.getProperty("fullscreen"));
+        int overrideWidth = Integer.parseInt(props.getProperty("overrideWidth"));
+        int overrideHeight = Integer.parseInt(props.getProperty("overrideHeight"));
+        int width = 854;
+        int height = 480;
+        if (overrideWidth > 0) {
+            width = overrideWidth;
+        } else if (args.containsKey("--width")) {
+            width = Math.max(1, Integer.parseInt(args.get("--width")));
+        }
+        if (overrideHeight > 0) {
+            height = overrideHeight;
+        } else if (args.containsKey("--height")) {
+            height = Math.max(1, Integer.parseInt(args.get("--height")));
+        }
+        String ver = args.get("--version");
+        if (ver == null) {
+            ver = "Unknown";
+        } else {
+            int index = ver.indexOf('-');
+            if (index != -1) {
+                ver = ver.substring(0, index);
+            }
+        }
+        try {
+            Display.setDisplayMode(new DisplayMode(width, height));
+            Display.setFullscreen(fullscreen);
+            Display.setResizable(true);
+            Display.setTitle("Minecraft "+ver);
+            try {
+                Display.create(new PixelFormat().withDepthBits(24));
+            } catch (LWJGLException e) {
+                e.printStackTrace();
+                Display.create();
+            }
+            GL11.glClearColor(1, 1, 1, 1);
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+            Display.update();
+        } catch (LWJGLException e1) {
+            e1.printStackTrace();
+            Throwables.propagate(e1);
         }
     }
 
