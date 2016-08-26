4912d3111d8c933deff129c2a5b135462098e2a9
modmuss50 <modmuss50@gmail.com>
2016-08-26 19:45:20 +0100

    Added server launcher

---
+++ src/main/java/net/fabricmc/base/launch/FabricServerLauncher.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.base.launch;
+
+import net.minecraft.launchwrapper.Launch;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FabricServerLauncher {
+
+	public static void main(String[] args) {
+
+		List<String> argList = new ArrayList<>();
+		for (String arg : args) {
+			argList.add(arg);
+		}
+		argList.add("--tweakClass");
+		argList.add("net.fabricmc.base.launch.FabricServerTweaker");
+
+		Object[] objectList = argList.toArray();
+		String[] stringArray = Arrays.copyOf(objectList, objectList.length, String[].class);
+		Launch.main(stringArray);
+	}
+
+}
