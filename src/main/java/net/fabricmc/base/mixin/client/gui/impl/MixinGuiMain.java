efe24303825fcc33c01cf58c2341588eca965b94
Shadowfacts <me@shadowfacts.net>
2016-08-29 15:09:05 -0400

    Update for latest mappings

--- src/main/java/net/fabricmc/base/mixin/client/gui/impl/MixinGuiMain.java
+++ src/main/java/net/fabricmc/base/mixin/client/gui/impl/MixinGuiMain.java
@@ -16,18 +16,18 @@
 
 package net.fabricmc.base.mixin.client.gui.impl;
 
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.screen.menu.GuiMainMenu;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.menu.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiMainMenu.class, remap = false)
-public abstract class MixinGuiMain extends GuiScreen {
+public abstract class MixinGuiMain extends Gui {
 
-    @Inject(method = "drawScreen(IIF)V", at = @At("RETURN"))
-    public void draw(int a1, int a2, float a3, CallbackInfo info) {
+    @Inject(method = "draw(IIF)V", at = @At("RETURN"))
+    public void draw(int mouseX, int mouseY, float delta, CallbackInfo info) {
         this.fontRenderer.drawString("Fabric Base", 2, this.height - 20, -1);
     }
 
--- src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
+++ src/test/java/net/fabricmc/test/mixin/MixinGuiMain.java
@@ -16,18 +16,18 @@
 
 package net.fabricmc.test.mixin;
 
-import net.minecraft.client.gui.GuiScreen;
-import net.minecraft.client.gui.screen.menu.GuiMainMenu;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.menu.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = GuiMainMenu.class, remap = false)
-public abstract class MixinGuiMain extends GuiScreen {
+public abstract class MixinGuiMain extends Gui {
 
-    @Inject(method = "drawScreen(IIF)V", at = @At("RETURN"))
-    public void draw(int a1, int a2, float a3, CallbackInfo info) {
+    @Inject(method = "draw(IIF)V", at = @At("RETURN"))
+    public void draw(int mouseX, int mouseY, float delta, CallbackInfo info) {
         this.fontRenderer.drawString("Fabric Test Mod", 2, this.height - 30, -1);
     }
 
