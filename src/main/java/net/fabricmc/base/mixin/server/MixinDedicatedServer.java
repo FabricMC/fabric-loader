bf51ea641c8bddb07a2c42e2d707afc5a6c402ab
asie <kontakt@asie.pl>
2018-10-26 08:08:31 +0200

    restore dedicated server support

--- src/main/java/net/fabricmc/base/mixin/server/MixinDedicatedServer.java
+++ src/main/java/net/fabricmc/base/mixin/server/MixinDedicatedServer.java
@@ -19,31 +19,42 @@ package net.fabricmc.base.mixin.server;
 import com.mojang.authlib.GameProfileRepository;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import com.mojang.datafixers.DataFixer;
 import net.fabricmc.base.Fabric;
 import net.fabricmc.base.loader.Loader;
 import net.fabricmc.base.server.ServerSidedHandler;
+import net.minecraft.command.CommandManagerServer;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.util.UserCache;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
 import java.net.Proxy;
 
-// FIXME
-
-//@Mixin($root.th.class)
-public abstract class MixinDedicatedServer /* extends MinecraftServer */ {
-	/* public MixinDedicatedServer(File a1, Proxy a2, MigrationHandler a3, YggdrasilAuthenticationService a4, MinecraftSessionService a5, GameProfileRepository a6, UserCache a7) {
-		super(a1, a2, a3, a4, a5, a6, a7);
-	} */
-
-//	@Inject(method = "setupServer", at = @At("HEAD"))
-	public void j(CallbackInfoReturnable<Boolean> info) throws IOException {
-		/* Fabric.initialize(this.getFile(""), new ServerSidedHandler(this));
-		Loader.INSTANCE.load(this.getFile("mods")); */
+@Mixin(DedicatedServer.class)
+public abstract class MixinDedicatedServer extends MinecraftServer {
+	public MixinDedicatedServer(
+		@Nullable
+			File aFile1,
+		Proxy aProxy2,
+		DataFixer aDataFixer3,
+		CommandManagerServer aCommandManagerServer4,
+		YggdrasilAuthenticationService aYggdrasilAuthenticationService5,
+		MinecraftSessionService aMinecraftSessionService6,
+		GameProfileRepository aGameProfileRepository7,
+		UserCache aUserCache8) {
+		super(aFile1, aProxy2, aDataFixer3, aCommandManagerServer4, aYggdrasilAuthenticationService5, aMinecraftSessionService6, aGameProfileRepository7, aUserCache8);
 	}
 
+	@Inject(method = "setupServer", at = @At("HEAD"))
+	public void j(CallbackInfoReturnable<Boolean> info) throws IOException {
+		Fabric.initialize(this.getFile(""), new ServerSidedHandler(this));
+		Loader.INSTANCE.load(this.getFile("mods"));
+	}
 }
