2435fafd156a5c8dc1369d16a509ee33a59f6e88
Erlend Åmdal <erlend@aamdal.com>
2018-12-28 14:51:14 +0100

    Add javadocs for environment annotations and missing license headers

--- src/main/java/net/fabricmc/api/Environment.java
+++ src/main/java/net/fabricmc/api/Environment.java
@@ -16,10 +16,18 @@
 
 package net.fabricmc.api;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Applied to declare that the element is present only in the specified environment.
+ * Use with caution, as Fabric-loader will completely remove annotated elements in a mismatched environment!
+ * This annotation should be inherited when overriding a method.
+ */
 @Retention(RetentionPolicy.CLASS)
+@Target({ ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR })
 public @interface Environment {
 	EnvType value();
 }
--- src/main/java/net/fabricmc/api/EnvironmentInterfaces.java
+++ src/main/java/net/fabricmc/api/EnvironmentInterfaces.java
@@ -1,9 +1,33 @@
+/*
+ * Copyright 2016 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.api;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * Applied to declare that interface implementations are present only in the specified environment.
+ * Use with caution, as Fabric-loader will completely remove interface implementations in a mismatched environment!
+ * Implemented methods are not removed. To remove implemented methods, use {@link Environment}.
+ */
 @Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
 public @interface EnvironmentInterfaces {
 	EnvironmentInterface[] value();
 }
