befba663b65380b45a4331b2388ee2e4eb6ebbbe
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2019-03-23 11:56:59 +0100

    Remove test main method from JacksonSettings

--- src/main/java/net/fabricmc/api/settings/JacksonSettings.java
+++ src/main/java/net/fabricmc/api/settings/JacksonSettings.java
@@ -4,6 +4,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class JacksonSettings extends Settings<Object> {
+
+	public JacksonSettings(String name) {
+		super(name);
+	}
+
+	public JacksonSettings() {
+		super();
+	}
+
     @Override
     public void serialise(InputStream stream) {
 
@@ -12,40 +21,11 @@ public class JacksonSettings extends Settings<Object> {
     @Override
     public void deserialise(OutputStream stream) {
 
-    }
-
-    public JacksonSettings(String name) {
-        super(name);
-    }
-
-    public JacksonSettings() {
-        super();
-    }
+	}
 
     @Override
     protected Settings createSub(String name) {
         return new JacksonSettings(name);
-    }
-
-    public static void main(String[] args) {
-        JacksonSettings settings = new JacksonSettings();
-
-        settings.set("Setting 1", "Hello world!");
-
-        Setting<String> setting = settings
-                .builder(String.class)
-                .name("Setting 1")
-                .defaultValue("Default value")
-                .build();
-
-        Setting<String> setting2 = settings
-                .builder(String.class)
-                .name("Setting 2")
-                .defaultValue("Default value")
-                .build();
-
-        System.out.println(setting.getValue());
-        System.out.println(setting2.getValue());
     }
 
 }
