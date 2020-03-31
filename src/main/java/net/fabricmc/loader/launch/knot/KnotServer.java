1dae6eb949cd263b75cb02591f9c1f3665744681
modmuss50 <modmuss50@gmail.com>
2020-03-31 15:31:59 +0100

    Start looking at a custom log4j2 config.

--- src/main/java/net/fabricmc/loader/launch/knot/KnotServer.java
+++ src/main/java/net/fabricmc/loader/launch/knot/KnotServer.java
@@ -22,6 +22,7 @@ import java.io.File;
 
 public class KnotServer {
 	public static void main(String[] args) {
+		System.setProperty("log4j.configurationFile", "classpath:log4j2.fabric.xml");
 		String gameJarPath = System.getProperty("fabric.gameJarPath");
 		new Knot(EnvType.SERVER, gameJarPath != null ? new File(gameJarPath) : null).init(args);
 	}
