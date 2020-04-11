458f2f40b259a8ace3aa303c1dc39c08a42fd1c8
shedaniel <daniel@shedaniel.me>
2020-04-11 18:20:08 +0800

    revert guava fix

--- src/main/java/net/fabricmc/loader/transformer/FabricTransformer.java
+++ src/main/java/net/fabricmc/loader/transformer/FabricTransformer.java
@@ -21,13 +21,40 @@ import net.fabricmc.loader.FabricLoader;
 import net.fabricmc.loader.game.MinecraftGameProvider;
 import net.fabricmc.loader.launch.common.FabricLauncherBase;
 import net.fabricmc.loader.transformer.accesswidener.AccessWidenerVisitor;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.*;
 
 public final class FabricTransformer {
+
+	public static byte[] fixGuava(byte[] bytes) {
+		ClassWriter writer = new ClassWriter(0);
+		new ClassReader(bytes).accept(new ClassVisitor(Opcodes.ASM7, writer) {
+			@Override
+			public MethodVisitor visitMethod(int access, String name, String descriptor, String signature, String[] exceptions) {
+				return new MethodVisitor(api, super.visitMethod(access, name, descriptor, signature, exceptions)) {
+					@Override
+					public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+						if (owner.equals("com/google/common/base/Objects")
+						    && name.equals("firstNonNull")
+						    && descriptor.equals("(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;")) {
+							owner = "com/google/common/base/MoreObjects";
+						} else if (owner.equals("com/google/common/collect/Iterators")
+						           && name.equals("emptyIterator")) {
+							owner = "net/fabricmc/loader/guava/IteratorsFix";
+							name = "emptyListIterator";
+							descriptor = "()Lcom/google/common/collect/UnmodifiableListIterator;";
+						}
+
+						super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
+					}
+				};
+			}
+		}, 0);
+		return writer.toByteArray();
+	}
+
 	public static byte[] lwTransformerHook(String name, String transformedName, byte[] bytes) {
+		bytes = fixGuava(bytes);
+
 		boolean isDevelopment = FabricLauncherBase.getLauncher().isDevelopment();
 		EnvType envType = FabricLauncherBase.getLauncher().getEnvironmentType();
 
@@ -45,6 +72,8 @@ public final class FabricTransformer {
 	}
 
 	public static byte[] transform(boolean isDevelopment, EnvType envType, String name, byte[] bytes) {
+		bytes = fixGuava(bytes);
+
 		boolean isMinecraftClass = name.startsWith("net.minecraft.") || name.indexOf('.') < 0;
 		boolean transformAccess = isMinecraftClass && FabricLauncherBase.getLauncher().getMappingConfiguration().requiresPackageAccessHack();
 		boolean environmentStrip = !isMinecraftClass || isDevelopment;
