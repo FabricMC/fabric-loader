a22a07e00c85776e552f466277a14328bc529fb5
i509VCB <git@i509.me>
2020-06-18 16:39:45 -0500

    Stupid import

--- src/main/java/net/fabricmc/loader/metadata/CustomValueImpl.java
+++ src/main/java/net/fabricmc/loader/metadata/CustomValueImpl.java
@@ -17,6 +17,7 @@
 package net.fabricmc.loader.metadata;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
