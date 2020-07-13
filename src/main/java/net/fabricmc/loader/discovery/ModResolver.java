89aad6fcc609d1c1822b715bc412aa3e27708ef8
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2020-07-13 20:46:51 +0300

    Fix location of exclamation mark

--- src/main/java/net/fabricmc/loader/discovery/ModResolver.java
+++ src/main/java/net/fabricmc/loader/discovery/ModResolver.java
@@ -340,25 +340,25 @@ public class ModResolver {
 			errors.append("\n - Mod ").append(candidate.getInfo().getId()).append(" ").append(errorType).append(" mod ").append(dependency).append(", ");
 
 			if (depCandidate == null) {
-				errors.append("which is missing");
+				errors.append("which is missing!");
 				errors.append("\n - You must install " + depModId);
 			} else if (cond) {
-				errors.append("but a different version is present: ").append(depCandidate.getInfo().getVersion());
+				errors.append("but a different version is present: ").append(depCandidate.getInfo().getVersion()).append("!");
 				errors.append("\n - You must install version " + dependency.getInfo().getVersion() + " of " + depModId);
 			} else if (errorType.contains("conf")) {
 				// CONFLICTS WITH
-				errors.append("but the conflicting version is present: ").append(depCandidate.getInfo().getVersion());
+				errors.append("but the conflicting version is present: ").append(depCandidate.getInfo().getVersion()).append("!");
 				errors.append("\n - While this won't prevent you from starting the game, the developer(s) of " + candidate.getInfo.getId());
 				errors.append("\n   have found that version " + depCandidate.getInfo().getVersion() + " of ");
 				errors.append("\n   conflicts with their mod. It is heavily recommended to remove one of the mods.");
 			} else {
-				errors.append("but the breaking version is present: ").append(depCandidate.getInfo().getVersion());
+				errors.append("but the breaking version is present: ").append(depCandidate.getInfo().getVersion()).append("!");
 				errors.append("\n - The developer(s) of " + candidate.getInfo.getId())
 				errors.append("\n   have found that version " + depCandidate.getInfo().getVersion() + " of " + depModId);
 				errors.append("\n   critically conflicts with their mod. You must remove one of the mods.");
 			}
 
-			errors.append("!");
+			//errors.append("!");
 		}
 	}
 
