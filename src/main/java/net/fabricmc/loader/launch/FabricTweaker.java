35be2028270168730d73e3be4720abe55f09fba3
Jonas Herzig <me@johni0702.de>
2020-07-26 13:01:33 +0200

    Fix overwriting of third-party tranformations to entry point classes
    We store (`patchedClasses`) and then completely substitute during loading
    (`FabricTransformer.lwTranformerHook`) the bytecode for any classes which our
    `EntrypointTransformer`s need to change.
    
    Therefore, when they ask for any classes, we must explicitly pass the raw
    bytecode through all other transformers in order to not miss out on their
    changes.

--- src/main/java/net/fabricmc/loader/launch/FabricTweaker.java
+++ src/main/java/net/fabricmc/loader/launch/FabricTweaker.java
@@ -27,6 +27,7 @@ import net.fabricmc.loader.launch.common.FabricMixinBootstrap;
 import net.fabricmc.loader.util.Arguments;
 import net.fabricmc.loader.util.UrlConversionException;
 import net.fabricmc.loader.util.UrlUtil;
+import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.launchwrapper.LaunchClassLoader;
@@ -34,6 +35,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.transformer.Proxy;
 
 import java.io.File;
 import java.io.IOException;
@@ -174,19 +176,17 @@ public abstract class FabricTweaker extends FabricLauncherBase implements ITweak
 
 	@Override
 	public byte[] getClassByteArray(String name, boolean runTransformers) throws IOException {
-		if (runTransformers) {
-			// TODO: we need a way of applying the class transformers without applying mixins
-			throw new UnsupportedOperationException("TODO getClassByteArray/launchwrapper");
-		}
-
 		String transformedName = name.replace('/', '.');
 		byte[] classBytes = launchClassLoader.getClassBytes(name);
 
-//		if (runTransformers && !classLoaderUtil.isClassExcluded(name, transformedName)) {
-//			for (IClassTransformer transformer : launchClassLoader.getTransformers()) {
-//				classBytes = transformer.transform(name, transformedName, classBytes);
-//			}
-//		}
+		if (runTransformers) {
+			for (IClassTransformer transformer : launchClassLoader.getTransformers()) {
+				if (transformer instanceof Proxy) {
+					continue; // skip mixin as per method contract
+				}
+				classBytes = transformer.transform(name, transformedName, classBytes);
+			}
+		}
 
 		return classBytes;
 	}
