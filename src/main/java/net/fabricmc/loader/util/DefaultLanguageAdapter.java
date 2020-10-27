76191d4a935ba198474795b01683fadc32938d93
Haven <haven@hephaestus.dev>
2020-10-27 11:44:59 -0700

    Adds support for field and method entrypoints

--- src/main/java/net/fabricmc/loader/util/DefaultLanguageAdapter.java
+++ src/main/java/net/fabricmc/loader/util/DefaultLanguageAdapter.java
@@ -47,13 +47,11 @@ public final class DefaultLanguageAdapter implements LanguageAdapter {
 			throw new LanguageAdapterException(e);
 		}
 
-		if (type == Class.class) {
-			//noinspection unchecked
-			return (T) c;
-		}
-
 		if (methodSplit.length == 1) {
-			if (type.isAssignableFrom(c)) {
+			if (type == Class.class) {
+				//noinspection unchecked
+				return (T) c;
+			} else if (type.isAssignableFrom(c)) {
 				try {
 					//noinspection unchecked
 					return (T) c.getDeclaredConstructor().newInstance();
