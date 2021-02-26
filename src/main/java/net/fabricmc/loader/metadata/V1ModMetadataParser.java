a814378393c3e558871210401d742ff45b223e91
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-02-26 21:33:40 +0100

    Ignore $schema in v1 fabric.mod.json files instead of warning

--- src/main/java/net/fabricmc/loader/metadata/V1ModMetadataParser.java
+++ src/main/java/net/fabricmc/loader/metadata/V1ModMetadataParser.java
@@ -208,6 +208,9 @@ final class V1ModMetadataParser {
 			case "custom":
 				readCustomValues(reader, customValues);
 				break;
+			case "$schema":
+				reader.skipValue();
+				break;
 			default:
 				warnings.add(new ParseWarning(reader.getLineNumber(), reader.getColumn(), key, "Unsupported root entry"));
 				reader.skipValue();
