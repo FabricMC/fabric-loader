5f510756a89c5d71f76d3066fde4400b2b1e5759
Player <player@player.to>
2021-08-02 12:27:48 +0200

    Fork Java for displaying the error GUI if necessary (mac os)

---
+++ src/main/java/net/fabricmc/loader/impl/util/LoaderUtil.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.fabricmc.loader.impl.util;
+
+import java.util.Locale;
+
+public final class LoaderUtil {
+	public static boolean hasMacOs() {
+		return System.getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("mac");
+	}
+
+	public static boolean hasAwtSupport() {
+		if (hasMacOs()) {
+			// check for JAVA_STARTED_ON_FIRST_THREAD_<pid> which is set if -XstartOnFirstThread is used
+			// -XstartOnFirstThread is incompatible with AWT (force enables embedded mode)
+			for (String key : System.getenv().keySet()) {
+				if (key.startsWith("JAVA_STARTED_ON_FIRST_THREAD_")) return false;
+			}
+		}
+
+		return true;
+	}
+}
