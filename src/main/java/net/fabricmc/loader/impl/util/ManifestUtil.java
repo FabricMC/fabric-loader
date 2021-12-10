9ca9d98ceade8fb8a0914f6c632bccb6795863b8
Player <player@player.to>
2021-12-10 17:26:25 +0100

    Strip Jndi Lookup as necessary

--- src/main/java/net/fabricmc/loader/impl/util/ManifestUtil.java
+++ src/main/java/net/fabricmc/loader/impl/util/ManifestUtil.java
@@ -18,12 +18,45 @@ package net.fabricmc.loader.impl.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.JarURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.CodeSource;
 import java.util.jar.Attributes.Name;
 import java.util.jar.Manifest;
 
 public final class ManifestUtil {
+	public static Manifest readManifest(Class<?> cls) throws IOException, URISyntaxException {
+		CodeSource cs = cls.getProtectionDomain().getCodeSource();
+		if (cs == null) return null;
+
+		URL url = cs.getLocation();
+		if (url == null) return null;
+
+		return readManifest(url);
+	}
+
+	public static Manifest readManifest(URL codeSourceUrl) throws IOException, URISyntaxException {
+		Path path = UrlUtil.asPath(codeSourceUrl);
+
+		if (Files.isDirectory(path)) {
+			return readManifest(path);
+		} else {
+			URLConnection connection = new URL("jar:" + codeSourceUrl.toString() + "!/").openConnection();
+
+			if (connection instanceof JarURLConnection) {
+				return ((JarURLConnection) connection).getManifest();
+			}
+
+			try (FileSystemUtil.FileSystemDelegate jarFs = FileSystemUtil.getJarFileSystem(path, false)) {
+				return readManifest(jarFs.get().getRootDirectories().iterator().next());
+			}
+		}
+	}
+
 	public static Manifest readManifest(Path basePath) throws IOException {
 		Path path = basePath.resolve("META-INF").resolve("MANIFEST.MF");
 		if (!Files.exists(path)) return null;
