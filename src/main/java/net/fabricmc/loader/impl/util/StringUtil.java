9bdbdf88230e0575eebbc0fef95e1e5d9ddc9d42
Player <player@player.to>
2022-01-19 01:57:14 +0100

    Wrap lines in error gui, increase default width to 800 px

--- src/main/java/net/fabricmc/loader/impl/util/StringUtil.java
+++ src/main/java/net/fabricmc/loader/impl/util/StringUtil.java
@@ -40,4 +40,52 @@ public final class StringUtil {
 			return new String[] { defaultNamespace, s };
 		}
 	}
+
+	public static String wrapLines(String str, int limit) {
+		if (str.length() < limit) return str;
+
+		StringBuilder sb = new StringBuilder(str.length() + 20);
+		int lastSpace = -1;
+		int len = 0;
+
+		for (int i = 0, max = str.length(); i <= max; i++) {
+			char c = i < max ? str.charAt(i) : ' ';
+
+			if (c == '\r') {
+				// ignore
+			} else if (c == '\n') {
+				lastSpace = sb.length();
+				sb.append(c);
+				len = 0;
+			} else if (Character.isWhitespace(c)) {
+				if (len > limit && lastSpace >= 0) {
+					sb.setCharAt(lastSpace, '\n');
+					len = sb.length() - lastSpace - 1;
+				}
+
+				if (i == max) break;
+
+				if (len >= limit) {
+					lastSpace = -1;
+					sb.append('\n');
+					len = 0;
+				} else {
+					lastSpace = sb.length();
+					sb.append(c);
+					len++;
+				}
+			} else if (c == '"' || c == '\'') {
+				int next = str.indexOf(c, i + 1) + 1;
+				if (next <= 0) next = str.length();
+				sb.append(str, i, next);
+				len += next - i;
+				i = next - 1;
+			} else {
+				sb.append(c);
+				len++;
+			}
+		}
+
+		return sb.toString();
+	}
 }
