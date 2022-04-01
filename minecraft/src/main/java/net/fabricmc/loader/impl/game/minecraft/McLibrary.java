4d6a97fe5458dd88daf63e5934fcb730b0e72935
Player <player@player.to>
2022-04-01 12:50:29 +0200

    Swap QueueLogAppender for UUIDTypeAdapter in log4j plugin lib detection, the former is no longer in use

--- minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McLibrary.java
+++ minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McLibrary.java
@@ -30,9 +30,9 @@ enum McLibrary implements LibraryType {
 	LOG4J_API("org/apache/logging/log4j/LogManager.class"),
 	LOG4J_CORE("META-INF/services/org.apache.logging.log4j.spi.Provider", "META-INF/log4j-provider.properties"),
 	LOG4J_CONFIG("log4j2.xml"),
-	LOG4J_PLUGIN("com/mojang/util/QueueLogAppender.class"),
-	LOG4J_PLUGIN_2("com/mojang/patchy/LegacyXMLLayout.class"),
-	LOG4J_PLUGIN_3("net/minecrell/terminalconsole/util/LoggerNamePatternSelector.class"), // used by loom's log4j config
+	LOG4J_PLUGIN("com/mojang/util/UUIDTypeAdapter.class"), // in authlib
+	LOG4J_PLUGIN_2("com/mojang/patchy/LegacyXMLLayout.class"), // in patchy
+	LOG4J_PLUGIN_3("net/minecrell/terminalconsole/util/LoggerNamePatternSelector.class"), // in terminalconsoleappender, used by loom's log4j config
 	GSON("com/google/gson/TypeAdapter.class"), // used by log4j plugins
 	SLF4J_API("org/slf4j/Logger.class"),
 	SLF4J_CORE("META-INF/services/org.slf4j.spi.SLF4JServiceProvider");
