2a378f1c563b6ec96ae6620a278ecd23fa09da0f
modmuss50 <modmuss50@gmail.com>
2023-01-18 15:09:59 +0000

    Support 1.19.4 in McVersionLookup

--- minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McVersionLookup.java
+++ minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McVersionLookup.java
@@ -287,7 +287,9 @@ public final class McVersionLookup {
 			int year = Integer.parseInt(matcher.group(1));
 			int week = Integer.parseInt(matcher.group(2));
 
-			if (year == 22 && week >= 42 || year >= 23) {
+			if (year >= 23 && week >= 3) {
+				return "1.19.4";
+			} else if (year == 22 && week >= 42) {
 				return "1.19.3";
 			} else if (year == 22 && week == 24) {
 				return "1.19.1";
