98520668de90af9066337d2f2e8e241b27ffbcc9
modmuss50 <modmuss50@gmail.com>
2023-02-04 17:05:27 +0000

    Checkstyle and headers

--- minecraft/minecraft-test/src/test/java/net/fabricmc/minecraft/test/junit/JunitTest.java
+++ minecraft/minecraft-test/src/test/java/net/fabricmc/minecraft/test/junit/JunitTest.java
@@ -1,7 +1,26 @@
+/*
+ * Copyright 2016 FabricMC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.fabricmc.minecraft.test.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import net.minecraft.Bootstrap;
 import net.minecraft.SharedConstants;
@@ -10,8 +29,6 @@ import net.minecraft.block.GrassBlock;
 import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 public class JunitTest {
 	@BeforeAll
