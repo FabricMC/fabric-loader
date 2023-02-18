9f4ba698707b8c87aa6dba5ec4bdd320c1b7fda3
haykam821 <24855774+haykam821@users.noreply.github.com>
2023-02-18 10:26:20 -0500

    Fix icon objects in mod metadata not being parsed correctly (#743)
    * Fix icon objects in mod metadata not being parsed correctly
    
    Fixes #741
    
    * Add mod metadata parsing tests for icons

--- src/main/java/net/fabricmc/loader/impl/metadata/V1ModMetadataParser.java
+++ src/main/java/net/fabricmc/loader/impl/metadata/V1ModMetadataParser.java
@@ -595,10 +595,6 @@ final class V1ModMetadataParser {
 			final SortedMap<Integer, String> iconMap = new TreeMap<>(Comparator.naturalOrder());
 
 			while (reader.hasNext()) {
-				if (reader.peek() != JsonToken.STRING) {
-					throw new ParseMetadataException("Icon path must be a string", reader);
-				}
-
 				String key = reader.nextName();
 
 				int size;
@@ -612,6 +608,12 @@ final class V1ModMetadataParser {
 				if (size < 1) {
 					throw new ParseMetadataException("Size must be positive!", reader);
 				}
+
+				if (reader.peek() != JsonToken.STRING) {
+					throw new ParseMetadataException("Icon path must be a string", reader);
+				}
+
+				iconMap.put(size, reader.nextString());
 			}
 
 			reader.endObject();
