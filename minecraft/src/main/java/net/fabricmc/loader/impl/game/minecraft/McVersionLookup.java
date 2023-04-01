69fefcb72af0598524b1468028b3a93202dc517b
modmuss50 <modmuss50@gmail.com>
2023-04-01 11:45:24 +0100

    Support 23w13a_or_b

--- minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McVersionLookup.java
+++ minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McVersionLookup.java
@@ -56,7 +56,7 @@ public final class McVersionLookup {
 			+ "(Alpha|Beta) v?\\d+\\.\\d+(\\.\\d+)?[a-z]?(_\\d+)?[a-z]?|" // long alpha/beta names: Alpha v1.2.3_45
 			+ "Inf?dev (0\\.31 )?\\d+(-\\d+)?|" // long indev/infdev names: Infdev 12345678-9
 			+ "(rd|inf)-\\d+|" // early rd-123, inf-123
-			+ "1\\.RV-Pre1|3D Shareware v1\\.34|" // odd exceptions
+			+ "1\\.RV-Pre1|3D Shareware v1\\.34|23w13a_or_b|" // odd exceptions
 			+ "(.*[Ee]xperimental [Ss]napshot )(\\d+)" // Experimental versions.
 			);
 	private static final Pattern RELEASE_PATTERN = Pattern.compile("\\d+\\.\\d+(\\.\\d+)?");
@@ -588,6 +588,10 @@ public final class McVersionLookup {
 		case "2point0_blue":
 			// 2.0 update version blue, forked from 1.5.1
 			return "1.5.2-blue";
+
+		case "23w13a_or_b":
+			// Minecraft 23w13a_or_b, forked from 23w13a
+			return "1.20-alpha.23.13.ab";
 
 		default:
 			return null; //Don't recognise the version
