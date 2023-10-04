0ba1a7ee50f86a0bbfb2fc7b0a7b33db501211c5
modmuss50 <modmuss50@gmail.com>
2023-10-04 21:53:03 +0100

    Bump version

--- src/main/java/net/fabricmc/loader/impl/FabricLoaderImpl.java
+++ src/main/java/net/fabricmc/loader/impl/FabricLoaderImpl.java
@@ -73,7 +73,7 @@ public final class FabricLoaderImpl extends net.fabricmc.loader.FabricLoader {
 
 	public static final int ASM_VERSION = Opcodes.ASM9;
 
-	public static final String VERSION = "0.14.22";
+	public static final String VERSION = "0.14.23";
 	public static final String MOD_ID = "fabricloader";
 
 	public static final String CACHE_DIR_NAME = ".fabric"; // relative to game dir
