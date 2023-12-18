3f7cd30b1dd5228d8652c80f928dfec9fb8c9061
modmuss50 <modmuss50@gmail.com>
2023-12-18 15:17:08 +0000

    Support 1.20.5 snapshots

--- minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McVersionLookup.java
+++ minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/McVersionLookup.java
@@ -287,7 +287,9 @@ public final class McVersionLookup {
 			int year = Integer.parseInt(matcher.group(1));
 			int week = Integer.parseInt(matcher.group(2));
 
-			if (year >= 23 && week >= 40) {
+			if (year >= 23 && week >= 51) {
+				return "1.20.5";
+			} else if (year == 23 && week >= 40 && week <= 46) {
 				return "1.20.3";
 			} else if (year == 23 && week >= 31 && week <= 35) {
 				return "1.20.2";
