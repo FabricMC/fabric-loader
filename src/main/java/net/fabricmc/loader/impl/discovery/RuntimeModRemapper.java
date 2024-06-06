75d5f840bb882c032bb86c3801b56421a31bfb5f
Thomas <tomwmth@pm.me>
2024-06-06 14:46:57 +0800

    Use FileSystemDelegate in try-with-resources
    This exact method is called twice later in this same function and always with a try-with-resources. Update this occurrence to match rather than use an explicit close call.

--- src/main/java/net/fabricmc/loader/impl/discovery/RuntimeModRemapper.java
+++ src/main/java/net/fabricmc/loader/impl/discovery/RuntimeModRemapper.java
@@ -122,18 +122,18 @@ public final class RuntimeModRemapper {
 				RemapInfo info = infoMap.get(mod);
 				OutputConsumerPath outputConsumer = new OutputConsumerPath.Builder(info.outputPath).build();
 
-				FileSystemUtil.FileSystemDelegate delegate = FileSystemUtil.getJarFileSystem(info.inputPath, false);
+				try (FileSystemUtil.FileSystemDelegate jarFs = FileSystemUtil.getJarFileSystem(info.inputPath, false)) {
+					if (jarFs.get() == null) {
+						throw new RuntimeException("Could not open JAR file " + info.inputPath.getFileName() + " for NIO reading!");
+					}
 
-				if (delegate.get() == null) {
-					throw new RuntimeException("Could not open JAR file " + info.inputPath.getFileName() + " for NIO reading!");
+					Path inputJar = jarFs.get().getRootDirectories().iterator().next();
+					outputConsumer.addNonClassFiles(inputJar, NonClassCopyMode.FIX_META_INF, remapper);
+
+					info.outputConsumerPath = outputConsumer;
+
+					remapper.apply(outputConsumer, info.tag);
 				}
-
-				Path inputJar = delegate.get().getRootDirectories().iterator().next();
-				outputConsumer.addNonClassFiles(inputJar, NonClassCopyMode.FIX_META_INF, remapper);
-
-				info.outputConsumerPath = outputConsumer;
-
-				remapper.apply(outputConsumer, info.tag);
 			}
 
 			//Done in a 3rd loop as this can happen when the remapper is doing its thing.
