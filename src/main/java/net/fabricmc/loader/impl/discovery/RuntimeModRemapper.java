1840e8df4a13bb5eaa823c29a09d260932496c26
Player <sfPlayer1@users.noreply.github.com>
2024-07-09 13:11:02 -0400

    Avoid jar cache when reading Manifest, reserve URL based method for special cases only (#936)

--- src/main/java/net/fabricmc/loader/impl/discovery/RuntimeModRemapper.java
+++ src/main/java/net/fabricmc/loader/impl/discovery/RuntimeModRemapper.java
@@ -222,8 +222,9 @@ public final class RuntimeModRemapper {
 	}
 
 	private static boolean requiresMixinRemap(Path inputPath) throws IOException, URISyntaxException {
-		final Manifest manifest = ManifestUtil.readManifest(inputPath.toUri().toURL());
+		final Manifest manifest = ManifestUtil.readManifest(inputPath);
 		final Attributes mainAttributes = manifest.getMainAttributes();
+
 		return REMAP_TYPE_STATIC.equalsIgnoreCase(mainAttributes.getValue(REMAP_TYPE_MANIFEST_KEY));
 	}
 
