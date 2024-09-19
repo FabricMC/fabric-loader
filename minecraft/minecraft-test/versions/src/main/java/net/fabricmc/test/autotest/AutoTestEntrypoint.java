ccb2d319e48879a08bd1d76e859178eed420fea5
modmuss50 <modmuss50@gmail.com>
2024-09-19 22:41:46 +0100

    Setup basic shared auto test mod

---
+++ minecraft/minecraft-test/versions/src/main/java/net/fabricmc/test/autotest/AutoTestEntrypoint.java
@@ -0,0 +1,10 @@
+package net.fabricmc.test.autotest;
+
+import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+
+public class AutoTestEntrypoint implements PreLaunchEntrypoint {
+	@Override
+	public void onPreLaunch() {
+
+	}
+}
